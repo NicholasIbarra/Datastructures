856d58e0d5eea116fd926b8b3f58e3cf706a82ce
Nicholas Ibarra <nlibarra12@gmail.com>
2021-08-09 22:03:54 -0500

    BstIterator

--- Algorithms/Solutions/BinaryTreeMaximumPathSum.cs
+++ Algorithms/Solutions/BinaryTreeMaximumPathSum.cs
@@ -4,21 +4,21 @@ using System.Text;
 
 namespace Datastructure.Algorithms.Solutions
 {
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
-
     public class BinaryTreeMaximumPathSum
     {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        
         public static void Test()
         {
             TreeNode root = new TreeNode(-10)
